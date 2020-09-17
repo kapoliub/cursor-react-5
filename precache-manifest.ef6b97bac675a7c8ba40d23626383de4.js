8c6068dea57c5d4f10b2c805195f18b9d51f5a44
Liubomyr <louidropson@gmail.com>
2020-09-17 15:17:45 +0300

    Updates

---
+++ precache-manifest.ef6b97bac675a7c8ba40d23626383de4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14363ea2509d9f5129965ddd85d5f2f4",
+    "url": "/cursor-react-5/index.html"
+  },
+  {
+    "revision": "b1524d6501e6ff1fb716",
+    "url": "/cursor-react-5/static/css/main.0759738e.chunk.css"
+  },
+  {
+    "revision": "6906ef8a2998bc1e0cb2",
+    "url": "/cursor-react-5/static/js/2.db5c08fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cursor-react-5/static/js/2.db5c08fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1524d6501e6ff1fb716",
+    "url": "/cursor-react-5/static/js/main.5b26fadb.chunk.js"
+  },
+  {
+    "revision": "193781747e25b89760ab",
+    "url": "/cursor-react-5/static/js/runtime-main.0aff1afc.js"
+  },
+  {
+    "revision": "1cd146fe68ce9aae34927692e4110d28",
+    "url": "/cursor-react-5/static/media/arrow-logo.1cd146fe.svg"
+  },
+  {
+    "revision": "6344ee7bdd3f87d30add1b9a5f1422f0",
+    "url": "/cursor-react-5/static/media/comment_icon.6344ee7b.svg"
+  },
+  {
+    "revision": "71c96efa460097646fcc363720eb9448",
+    "url": "/cursor-react-5/static/media/female-icon.71c96efa.png"
+  },
+  {
+    "revision": "57c9a039101ba4c5d3e5a7cc8b541f52",
+    "url": "/cursor-react-5/static/media/like_icon.57c9a039.svg"
+  },
+  {
+    "revision": "6aac446dc5f69e5a132c6a35e9875d41",
+    "url": "/cursor-react-5/static/media/male-icon.6aac446d.png"
+  },
+  {
+    "revision": "a9a99fc3841f1690924d6d94a37bc78e",
+    "url": "/cursor-react-5/static/media/repost_icon.a9a99fc3.svg"
+  },
+  {
+    "revision": "3161a5f972f7f45c01ef6cff577559e8",
+    "url": "/cursor-react-5/static/media/search-icon.3161a5f9.png"
+  },
+  {
+    "revision": "8e82847c0b2cfcb461c4dadefb5b59d0",
+    "url": "/cursor-react-5/static/media/share_icon.8e82847c.svg"
+  },
+  {
+    "revision": "2019cadf6bdc5d95f29a232dff10f6ea",
+    "url": "/cursor-react-5/static/media/verified-logo.2019cadf.svg"
+  }
+]);
